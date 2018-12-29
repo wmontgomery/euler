6b358174b981ba6a0840f990c7aec2da93ab9295
will <wmontgomery85@gmail.com>
2018-12-28 23:28:22 -0500

    break it up a little more

--- problem1/index.js
+++ problem1/index.js
@@ -4,13 +4,14 @@ If we list all the natural numbers below 10 that are multiples of 3 or 5, we get
 Find the sum of all the multiples of 3 or 5 below 1000.
 */
 
+const isMultipleOf = (myNumber) => 
+  myNumber % 3 === 0 || myNumber % 5 === 0 ? myNumber : 0
 
 const getTheSum = (theIndex) => {
   let theSum = 0
+
   for( let i = 0; i < theIndex; i++ ) {
-    if ( i % 3 === 0 || i % 5 === 0 ) {
-      theSum += i
-    }
+    theSum += isMultipleOf(i)
   }
 
   return theSum
