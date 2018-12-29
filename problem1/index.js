2b0080f5b48c2659baea275947ff357dd248721f
will <wmontgomery85@gmail.com>
2018-12-28 23:20:41 -0500

    Problem 1 of the Euler Project. Can make this better I know

---
+++ problem1/index.js
@@ -0,0 +1,19 @@
+/*
+If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9. The sum of these multiples is 23.
+
+Find the sum of all the multiples of 3 or 5 below 1000.
+*/
+
+
+const getTheSum = (theIndex) => {
+  let theSum = 0
+  for( let i = 0; i < theIndex; i++ ) {
+    if ( i % 3 === 0 || i % 5 === 0 ) {
+      theSum += i
+    }
+  }
+
+  return theSum
+}
+
+console.log( getTheSum(process.argv[2]))
